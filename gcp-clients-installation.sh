dbd613d89d1c13a978db2bb5ef035168bfd4cd34
e00049 <105837578+e00049@users.noreply.github.com>
2024-02-18 22:57:59 +0530

    gcp-sdk-clients-installation.sh

---
+++ gcp-clients-installation.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+sudo useradd -s /bin/bash -d /home/e00049 -m e00049
+sudo echo -e "kubectl\nkubectl" | passwd e00049 > /dev/null 2>&1
+echo "e00049 ALL=(ALL)      NOPASSWD: ALL" >> /etc/sudoers
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin forced-commands-only/#PermitRootLogin forced-commands-only/g' /etc/ssh/sshd_config
+systemctl restart sshd >/dev/null 2>&1
+
+sudo apt-get install apt-transport-https ca-certificates gnupg -y  && \
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
+sudo apt-get update && sudo apt-get install google-cloud-cli -y && \
+sudo apt-get install google-cloud-cli* -y
+
+
+gcloud auth activate-service-account --key-file=e00049-projects-workspace.json
+export GOOGLE_APPLICATION_CREDENTIALS="/tmp/e00049-projects-workspace.json"
+
+---------------------------------------------------------------------------------------
+# Using gcloud command:
+
+- LIST OUT the buckers:
+	gcloud storage ls
+		
+-------------------------------------------------------------------------------------
+
+# Install Node.JS
+
+sudo apt update && \
+sudo apt-get install build-essential -y && \
+curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash - && \
+sudo apt-get install -y nodejs
+
+npm install @google-cloud/storage
+
+// --------------------------------------------------------------------------------------
+
+# Installing Python:
+sudo apt install python3-pip -y
+pip3 install google-cloud-storage
+
+// -----------------------------------------------------------------------------------------
+
+# Installal GoLang:
+	
+sudo wget https://dl.google.com/go/go1.22.0.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz	
+export PATH=$PATH:/usr/local/go/bin
+	
+go install google.golang.org/api/iterator@latest
+go install cloud.google.com/go/storage@latest
+go get -u google.golang.org/api/iterator
+go get -u cloud.google.com/go/storage
+
+go mod init tidy
+
+
+// -------------------------------------------------------------------------------------------------------
+
+# install Terraform:
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform -y
+terraform -help
+
+terraform import google_storage_bucket.my-state-bucket e00049 
+
+terraform state list -> google_storage_bucket.my-state-bucket
+
+terraform state rm google_storage_bucket.my-state-bucket
+
+--------------------------------------------------------------------------------------------
