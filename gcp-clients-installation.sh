34c320223c565a81259d0f84f62f05ace72d4850
e00049 <105837578+e00049@users.noreply.github.com>
2024-06-02 11:01:11 +0530

    Update gcp-clients-installation.sh

--- gcp-clients-installation.sh
+++ gcp-clients-installation.sh
@@ -24,7 +24,6 @@ done
 
 systemctl restart ssh
 
-
 sudo apt-get install apt-transport-https ca-certificates gnupg -y  && \
 echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
